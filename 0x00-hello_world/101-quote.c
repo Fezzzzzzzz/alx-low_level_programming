f277e9d8a314ea3270fa3157c014343c31179f91
Mostafa Farrag <max40780@gmail.com>
2023-07-13 18:47:35 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
 */
 int main(void)
 {
-int A;
-A = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
