a5ffc36796b9e74631525483def5786cf3fd1635
Mostafa Farrag <max40780@gmail.com>
2023-07-14 16:34:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
