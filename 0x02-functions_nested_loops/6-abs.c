dccc66d3d3408be0e1af2a8e172046772c4b2f77
Mostafa Farrag <max40780@gmail.com>
2023-07-18 07:57:44 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sdlib.h>
+/**
+ * _islower- function that computes the absolute value of an integer.
+ *
+ * @c:the num to check
+ *
+ * Return: number
+ */
+int _abs(int c)
+{
+
+	return (abs(c));
+}
+
