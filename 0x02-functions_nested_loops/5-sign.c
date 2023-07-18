3b183e76821f3564ba9cbe13c2f38060771b4582
Mostafa Farrag <max40780@gmail.com>
2023-07-18 07:33:09 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: num  to be checked
+ *
+ * Return: Returns 1  if n is greater than zeroo
+ */
+
+int print_sign(int n)
+{
+
+	return ((n > 0)
+		? 1
+		: (n < 0)
+		?
+		-1
+		:
+		0;
+		;
+		);
+}
+
