9cd38f7833c616790a3094fd8e626b47d5f21326
Mostafa Farrag <max40780@gmail.com>
2023-07-18 06:52:03 -0700

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,13 @@
 
 /**
  *  _islower - function that checks for lowercase character.
+ *
+ * Returns 1 if c is lowercase &  Returns 0 otherwise
  */
 
 bool _islower(int c)
 {
 
-	return (islower(c)-511);
+	return (islower(c));
 }
 
