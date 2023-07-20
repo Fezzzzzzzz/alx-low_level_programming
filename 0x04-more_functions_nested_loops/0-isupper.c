3306b3ed7fa3b80e5a92ef110ea9451b7531eb78
Mostafa Farrag <max40780@gmail.com>
2023-07-20 02:29:08 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,11 @@
 #include "main.h"
 /**
  * _isupper- checks for uppercase character
- *
  * @c:character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 bool _isupper(int c)
 {
-
 		return (isupper(c));
 }
