8545a60a05ec9f64d9205a213c492dbb3c838561
Mostafa Farrag <max40780@gmail.com>
2023-07-20 11:45:59 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * main - prints Fizz for multiples of 3, Buzz for multiples of 5, and FizzBuzz
+ * for multiples of both 3 and 5. Otherwise, prints the number.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+			continue;
+		}
+		if (i >= 10)
+			putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(' ');
+	}
+	return (0);
+}
