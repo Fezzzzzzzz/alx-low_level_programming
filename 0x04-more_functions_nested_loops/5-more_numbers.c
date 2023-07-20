a0b1c0d55f0f2e61c8c44f89918f0bbcbf7652a1
Mostafa Farrag <max40780@gmail.com>
2023-07-20 04:17:28 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers- function that prints the numbers, from 0 to 14
+ *
+ * Return: nothing
+ */
+
+
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			printf("%d", j);
+		}
+	putchar('\n');
+	}
+}
