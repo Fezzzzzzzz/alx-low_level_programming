dc197b25b749c20260e50767970cb14fe208b5d5
Mostafa Farrag <max40780@gmail.com>
2023-07-20 04:49:02 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line- function that prints line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
