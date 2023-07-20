e8590f56a789c8dd74266221cac456070c32ea43
Mostafa Farrag <max40780@gmail.com>
2023-07-20 02:26:10 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdbool.h>
-
+#include "main.h"
 /**
  * _isupper- checks for uppercase character
  *
- * Description: function that checks for uppercase character.
  * @c:character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
