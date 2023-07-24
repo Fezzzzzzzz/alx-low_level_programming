89dead382c1f500306c4a571aaf29dbd110e85bf
Mostafa Farrag <max40780@gmail.com>
2023-07-24 12:44:52 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string
+ * @str: pointer of string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i])
+		i++;
+	j = i / 2;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
