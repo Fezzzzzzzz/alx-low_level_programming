4e4f4e9898a4b00e198c3474e41150c642fb44a1
Mostafa Farrag <max40780@gmail.com>
2023-07-24 03:21:09 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98  -function that takes a pointer updates the value it points to to 98
- *@*n:  thr poiner
+ * reset_to_98  -function that takes a pointer
+ * updates the value it points to to 98
+ *@n:  the pointer
  *
  * return: void
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
