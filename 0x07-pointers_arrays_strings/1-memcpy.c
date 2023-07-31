9348b1fe999ca5f22be1d89d3a70543d33597d1f
Mostafa Farrag <max40780@gmail.com>
2023-07-31 12:26:28 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies from memory area src to memory area dest
+ * @dest:  pointer to the destination array where the content is to be copied
+ * @src: pointer to the source of data to be copied
+ * @n:  number of bytes
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
