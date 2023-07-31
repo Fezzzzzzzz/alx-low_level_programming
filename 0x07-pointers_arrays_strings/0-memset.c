b5e4d2095a6ef9ce5f02ef79666683ca8c466ac5
Mostafa Farrag <max40780@gmail.com>
2023-07-31 11:11:11 -0700

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
+ * _memset - function that fills memory with a constant byte.
+ * @s: a pointer
+ * @b: constant byte b
+ * @n:  number of bytes
  *
- *
- *
+ * Return: a pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
