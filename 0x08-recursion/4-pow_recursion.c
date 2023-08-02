e7bf8616034c630b32a3f371fc495b47bdf29d5b
Mostafa Farrag <max40780@gmail.com>
2023-08-02 05:21:19 -0700

    commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _pow_recursion - function that returns the value of x raised to  y
+ *@x: base
+ *@y: exponent
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (0);
+}
