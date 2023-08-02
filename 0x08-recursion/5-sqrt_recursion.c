8e12b6b6552a5e2ae81474c6b418a9270d06db08
Mostafa Farrag <max40780@gmail.com>
2023-08-02 08:47:35 -0700

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * helper - function that help to fine the root
+ * @n: the number
+ * @j: the number
+ * @i: num to increment
+ * Return: the root
+ */
+
+int helper(int n, int j)
+{
+	if (n < 0)
+		return (-1);
+	int i = 0;
+
+up:;
+	n = (n + (j / n)) / 2;
+	if (n * n == j)
+	{
+		return (n);
+	}
+	else
+	{
+		if (i > 100)
+			return (-1);
+	i++;
+	goto up;
+	}
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: the number
+ * Return: the root
+ */
+int _sqrt_recursion(int n)
+{
+	return (helper(n, n));
+}
