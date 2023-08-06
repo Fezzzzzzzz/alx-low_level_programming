b2908b9f5f667afc4fc55e905908b67c79c4058b
Mostafa Farrag <max40780@gmail.com>
2023-08-06 05:38:38 -0700

    commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,12 +3,12 @@
  * main - prints the arguments of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * @i: an integer
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	printf("%s\n", argv[i]);
 	return (0);
