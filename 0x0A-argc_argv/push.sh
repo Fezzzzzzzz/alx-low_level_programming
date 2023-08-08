7ebf8748ecb9f27bd5626a8bf74a8f8dc3812712
Mostafa Farrag <max40780@gmail.com>
2023-08-08 02:51:09 -0700

    add: 0-create_array.c

--- 0x0A-argc_argv/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Description:
-read commit 
-git add .
-git commit -m "$commit"
-git push
-
---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars, and initializes it with a specific
+ * char
+ * @size: size of the array
+ * @c: char to initialize the array with
+ *
+ * Return: pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ar =  malloc(size * sizeof(char));
+	while (!ar[i])
+	{
+		ar[i] = c;
+		i++;
+	}
+		return (ar);
+}
