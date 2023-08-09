d16d0b64e4cc72df4841036301d0de0c345f04d8
Mostafa Farrag <max40780@gmail.com>
2023-08-09 14:21:03 -0700

    fix:  treat NULL as empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,8 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int len1, len2;
 
+	s1 == NULL ? s1 = "" : s1;
+	s2 == NULL ? s2 = "" : s2;
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
