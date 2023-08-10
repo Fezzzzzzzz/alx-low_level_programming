40a618fb56426576087d9fb3b8e6d92ff29df706
Mostafa Farrag <max40780@gmail.com>
2023-08-10 11:01:46 -0700

    fix: If nmemb or size is 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int i;
 
+	if (size == 0 || nmemb == 0)
+		return (NULL);
 	array = malloc(size * nmemb);
 	if (!array)
 		return (NULL);
