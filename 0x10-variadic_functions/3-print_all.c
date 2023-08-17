fbd761b999f7393def083cf68f0eb44d3d089389
Mostafa Farrag <max40780@gmail.com>
2023-08-17 16:36:37 -0700

    add: 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,75 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-typedef struct {
-	char a;
-	void (*b)(va_list);
-} helper;
+#include <stdlib.h>
+/**
+ * print_int - prints a character
+ * @args: va_list containing the character to print
+ */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
+/**
+ * print_float - print
+ * @args: va_list containing the character to print
+ */
 void print_float(va_list args)
 {
-        printf("%f", va_arg(args, double));
+	printf("%f", va_arg(args, double));
 }
+/**
+ * print_char - print
+ * @args: va_list containing the character to print
+ */
 void print_char(va_list args)
 {
-        printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
+/**
+ * print_string - print
+ * @args: va_list containing the character to print
+ */
 void print_string(va_list args)
 {
 	char *a = va_arg(args, char *);
-	if (a != NULL)
-		printf("%s", a);
-	else
-		printf("(nil)");
-}
-void (*get(char a))(va_list)
-{
-	helper help[] = {
-		{'i', print_int},
-		{'c', print_char},
-		{'f', print_float},
-		{'s', print_string}
-	};	
-	int i = 0;
-	while (i < 4)
-	{
-	if (a == help[i].a)
-		return help[i].b;
-	i++;
-	}
-	return 0;
-}
 
+	printf("%s", (a != NULL) ? a : "(nil)");
+}
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
 void print_all(const char * const format, ...)
 {
+	helper help[] = {
+	{'i', print_int},
+	{'c', print_char},
+	{'f', print_float},
+	{'s', print_string}
+	};
 	va_list args;
-	int i = 0;
+	int i = 0, j = 0;
 
 	va_start(args, format);
-	while (format[i])
+	while (format && format[i])
 	{
-		get(format[i]);
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == help[j].a)
+			{
+				help[j].b(args);
+				if (format[i] + 2 != '\0')
+				{
+					printf(", ");
+				}
+			}
+			j++;
+		}
 		i++;
 	}
+	printf("\b \b\b \b");
 	va_end(args);
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 #include <stdarg.h>
+/**
+ * struct helper  - struct for printing different types
+ * @a: format specifier
+ * @b: function to print
+ */
+typedef struct helper
+{
+	int a;
+	void (*b)(va_list);
+} helper;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
