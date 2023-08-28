4e1a19d5c50efbe7e924dc286f9cfe07164db604
Mostafa Farrag <max40780@gmail.com>
2023-08-28 02:35:28 -0700

    fix: 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
 /**
  * helper - function that help to fine the root
- * @n: the number
- * @j: the number
- * @i: num to increment
+ * @n: integer
+ * @j: integer
  * Return: the root
  */
 
 int helper(int n, int j)
 {
-	int i = 0;
-	if (n < 0)
+	if (j * j == n)
+		return (j);
+	if (j * j > n)
 		return (-1);
-
-up:;
-	n = (n + (j / n)) / 2;
-	if (n * n == j)
-	{
-		return (n);
-	}
-	else
-	{
-		if (i > 100)
-			return (-1);
-	i++;
-	goto up;
-	}
+	return (helper(n, j + 1));
 }
-
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: the number
@@ -35,5 +21,7 @@ up:;
  */
 int _sqrt_recursion(int n)
 {
-	return (helper(n, n));
+	if (n < 0)
+		return (-1);
+	return (helper(n, 0));
 }
