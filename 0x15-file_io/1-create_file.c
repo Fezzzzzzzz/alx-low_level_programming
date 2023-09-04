82648710bf9071f63ab33cc9867b5888818944fb
Mostafa Farrag <max40780@gmail.com>
2023-09-04 14:53:41 -0700

    fix: if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
 	if (file == -1)
 		return (-1);
 	while (text_content[i])
